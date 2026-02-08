ffcd7f4793590c1d89f3f9f6d5758cfd998ff49e
vedikaawasthi2005 <vedikaawasthi2005@gmail.com>
2026-02-09 00:28:10 +0530

    app.js fixed for localhost

--- app.js
+++ app.js
@@ -122,6 +122,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log("app is listening to port 8080");
 });
